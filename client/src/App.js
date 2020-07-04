1f72c07b456f1eb5f41754650b7724cbf2523eca
LorenaMendes <lorenamendes.contato@gmail.com>
2020-07-04 14:24:29 -0300

    basic react modules added

---
+++ client/src/App.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import axios from 'axios';
+
+class App extends Component {
+  constructor(props){
+    super(props);
+    axios
+      .get('/')
+      .then(result => {
+        console.log('PÁGINA INICIAL');
+      })
+    
+      axios
+      .get('/search')
+      .then(result => {
+        console.log('REPOSITÓRIOS');
+      })
+  }
+  render(){
+
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+            >
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
